4c15633c7da12b1b3bed2fa66bdc18676dcc611b
Corentin MACE <corentin.mace@ynov.com>
2019-01-08 10:32:41 +0100

    V.0.0.5 - Re-organisation des fichiers, Ajout de commentaire dans le code

--- bot.js
+++ bot.js
@@ -1,13 +1,17 @@
 config = require('./config.json');
 const Discord = require('discord.js');
+const bot = new Discord.Client();
+
 let prefix = config.prefix;
 servToken = config.servToken;
-botToken = config.botToken
+botToken = config.botToken; // Creation des variables de la config
 
-const bot = new Discord.Client();
-const version = "ALPHA v0.0.4"
+const version = "ALPHA v0.0.5"; // Creation d'une variable de la version du bot
 
-function presence(num, bot){
+
+
+
+function presence(num, bot){ // Fonction du jeu du bot
 switch (num) {
   case 0:
     bot.user.setPresence({ game: { name: `Prefix : ${prefix} | ${prefix}help`, type: 0 } });
@@ -18,10 +22,12 @@ switch (num) {
   case 2:
     bot.user.setPresence({ game: { name: `${bot.guilds.get(servToken).memberCount} joueur !`, type: 0 } });
     break
+  case 3:
+    bot.user.setPresence({ game : { name: `Actuellement sur ${bot.guilds.size} serveurs !`}});
   }
 }
 
-function compteurPresence(num){
+function compteurPresence(num){ // Compteur pour changer le jeu du bot
   switch (num) {
     case 0:
       return 1
@@ -30,6 +36,9 @@ function compteurPresence(num){
       return 2;
       break;
     case 2:
+      return 3;
+      break;
+    case 3:
       return 0;
       break;
   }
@@ -38,12 +47,12 @@ function compteurPresence(num){
 bot.on('ready', () => {
   console.log(`Logged in as ${bot.user.tag}!`);
 
-  let compteur = 1;
+  let compteur = 1; // Compteur bot pour le jeu
 
 setInterval(function(){
   compteur = compteurPresence(compteur);
   presence(compteur, bot);
-}, 5000 );
+}, 5000 ); // Change le jeu du bot toute les 5s
 
 
 });
@@ -51,38 +60,40 @@ setInterval(function(){
 
 bot.on('message', message => {
 
-  if (message.content === '<@531552938171301888>') {
+  online = message.guild.members.filter(member => member.presence.status === "online").size // Filtrage des personnes en lignes
+
+  if (message.content === '<@531552938171301888>') { // Si l'utilisateur mentionne le bot
 
     message.channel.send(`Mon prefix est : ${prefix}`);
-    message.channel.send(`Si tu as besoin d'aide : ${prefix}help`)
+    message.channel.send(`Si tu as besoin d'aide : ${prefix}help`) // Repond avec le prefix et la commande d'aide
   }
 
-  if (message.content.startsWith(prefix + "prefix")) {
-    const args = message.content.slice(prefix.length).split(' ');
+  if (message.content.startsWith(prefix + "prefix")) { // Commande de changement de prefix [WIP]
+    const args = message.content.slice(prefix.length).split(' '); // Creation des arguments
     const command = args.shift().toLowerCase();
     console.log(args)
-    prefix = args[0];
-    message.channel.send(`Le prefix sélectionné est ${prefix}`);
+    prefix = args[0]; // Changement du prefix pour l'arg 0
+    message.channel.send(`Le prefix sélectionné est ${prefix}`); // Envoi d'un message pour prevenir quel eest le nouveau prefix
   }
 
-   if (message.member.hasPermission('BAN_MEMBERS') && message.content.startsWith(prefix + "server-name")) {
-      const args = message.content.slice(prefix.length).split(' ');
-      args.shift(args[0])
-    message.guild.setName(args.join(' '));
+   if (message.member.hasPermission('BAN_MEMBERS') && message.content.startsWith(prefix + "server-name")) { // Commande de changement du nom du serveur (Admin-only)
+      const args = message.content.slice(prefix.length).split(' '); // Creation des arguments
+      args.shift(args[0]) // On saute l'argument 0
+    message.guild.setName(args.join(' ')); // On relie les arguments avec un espace pour pouvoir mettre des espaces dans le nom
     console.log(args.join(' '));
     setTimeout(function(){ message.channel.send(`Le nom du serveur à été changé pour **${message.guild.name}**`);
-  }, 1500);
+  }, 1500); // On attends 1,5s pour envoyer le nouveau nom du serveur le temps que le nom soit prit en compte
 
     }
-    else if (!message.member.hasPermission('BAN_MEMBERS') && message.content.startsWith(prefix + "server-name")){
-    message.channel.send("Vous n'avez pas la permission de changer le nom du serveur !");
+    else if (!message.member.hasPermission('BAN_MEMBERS') && message.content.startsWith(prefix + "server-name")){ // Si la personne n'a pas les droits d'utiliser la commande
+    message.channel.send("Vous n'avez pas la permission de changer le nom du serveur !"); // Envoi d'un message qui montre qu'il n'a pas les droits
   }
 
 
-  if(message.content === prefix + "server-info"){
-    message.delete();
+  if(message.content === prefix + "server-info"){ // Commande d'information du serveur
+    message.delete(); // Supression de la commande d'invocation
 
-  let serverembed = new Discord.RichEmbed()
+  let serverembed = new Discord.RichEmbed() // Creation de l'embed
   .setDescription("**Informations du serveur**")
   .setColor(0xb4a734)
   .addField("Nom du serveur :", message.guild.name, true)
@@ -90,19 +101,19 @@ bot.on('message', message => {
   .addField("Rejoins le", message.member.joinedAt)
   .addField("Crée le", message.guild.createdAt)
   .addField("Nombres de membres", message.guild.memberCount, true)
+  .addField("En ligne :", online, true)
   .addField("Acronyme :", message.guild.nameAcronym, true)
   .addField("Region :", message.guild.region, true)
   .setFooter(`Demandé par ${message.author.username}`, message.author.avatarURL);
-  return message.channel.send(serverembed);
+  return message.channel.send(serverembed); // Envoi de l'embed
   }
 
-  if(message.content === prefix + "bot-info"){
-  message.delete();
+  if(message.content === prefix + "bot-info"){ // Commande d'information du bot
+  message.delete(); // Supression de la commande d'invocation
 
-  let serverembed = new Discord.RichEmbed()
+  let serverembed = new Discord.RichEmbed() // Creation de l'embed
   .setDescription("**Informations du bot**")
   .setColor(0xb4a734)
-
   .addField("Nom du Bot", bot.user.tag, true)
   .addField("Version", version, true)
   .addField("Prefix", prefix, true)
@@ -112,13 +123,13 @@ bot.on('message', message => {
   .setFooter(`Demandé par ${message.author.username}`, message.author.avatarURL);
 
 
-  return message.channel.send(serverembed);
+  return message.channel.send(serverembed); // Envoi de l'embed
   }
 
 
-  if (message.content === prefix +  "help") {
-    message.delete();
-  const embed = new Discord.RichEmbed()
+  if (message.content === prefix +  "help") { // Commande d'aides
+    message.delete(); // Supression de la commande d'invocation
+  const embed = new Discord.RichEmbed() // Creation de l'embed
     .setTitle("Liste des commandes :")
   .setColor(0xb4a734)
   .addField(`${prefix}help`, "Affiche la liste des commandes")
@@ -130,15 +141,15 @@ bot.on('message', message => {
   .addField(`${prefix}prefix`, "Permet de changer le prefix du serveur")
   .addField(`${prefix}server-name`, "Permet de changer de nom du serveur")
   .setFooter(`Demandé par ${message.author.username}`, message.author.avatarURL);
-  message.channel.send({embed});;
+  message.channel.send({embed});; // Envoi de l'embed
 
 
   }
-  if (message.content === prefix + 'races') {
-    message.delete();
+  if (message.content === prefix + 'races') { // Commande de l'envoi des races disponibles
+    message.delete(); // Supression de la commande d'invocation
 
-    message.channel.send("**Voici la liste des races disponibles :**")
-    let emb1 = new Discord.RichEmbed({})
+    message.channel.send("**Voici la liste des races disponibles :**") // Envoi d'un message
+    let emb1 = new Discord.RichEmbed({}) // Creation de l'embed
 
     .setTitle("Elfes")
     .setColor(0x1a6199)
@@ -151,7 +162,7 @@ bot.on('message', message => {
     .addField("Intuition :", "5/10")
     .addField("Charisme :", "7/10");
 
-    let emb2 = new Discord.RichEmbed({})
+    let emb2 = new Discord.RichEmbed({}) // Creation de l'embed
 
     .setTitle("Gnomes")
     .setColor(0x1a6199)
@@ -164,7 +175,7 @@ bot.on('message', message => {
     .addField("Intuition :", "8/10")
     .addField("Charisme :", "2/10");
 
-    let emb3 = new Discord.RichEmbed({})
+    let emb3 = new Discord.RichEmbed({}) // Creation de l'embed
 
     .setTitle("Humains")
     .setColor(0x1a6199)
@@ -177,7 +188,7 @@ bot.on('message', message => {
     .addField("Intuition :", "6/10")
     .addField("Charisme :", "6/10");
 
-    let emb4 = new Discord.RichEmbed({})
+    let emb4 = new Discord.RichEmbed({}) // Creation de l'embed
 
     .setTitle("Nains")
     .setColor(0x1a6199)
@@ -190,7 +201,7 @@ bot.on('message', message => {
     .addField("Intuition :", "6/10")
     .addField("Charisme :", "5/10");
 
-    let emb5 = new Discord.RichEmbed({})
+    let emb5 = new Discord.RichEmbed({}) // Creation de l'embed
 
     .setTitle("Orques")
     .setColor(0x1a6199)
@@ -204,14 +215,14 @@ bot.on('message', message => {
     .addField("Charisme :", "9/10");
 
   message.channel.send(emb1).then(msg => {message.channel.send(emb2);}).then(msg => {message.channel.send(emb3);}).then(msg => {message.channel.send(emb4);}).then(msg => {message.channel.send(emb5);});
-
+  // Envoi de tout les embeds
   }
 
-  if (message.content === prefix + 'classes') {
-    message.delete();
+  if (message.content === prefix + 'classes') { // Commande de l'envoi des classes
+    message.delete(); // Supression de la commande d'invocation
 
-    message.channel.send("**Voici la liste des classes disponibles :**")
-    let emb6 = new Discord.RichEmbed({})
+    message.channel.send("**Voici la liste des classes disponibles :**") // Envoi d'un message
+    let emb6 = new Discord.RichEmbed({}) // Creation de l'embed
 
     .setTitle("Archer")
     .setColor(0x991928)
@@ -221,7 +232,7 @@ bot.on('message', message => {
     .addField("Pouvoir :", "Perception accrue (15 Mana)")
 
 
-    let emb7 = new Discord.RichEmbed({})
+    let emb7 = new Discord.RichEmbed({}) // Creation de l'embed
 
     .setTitle("Druide")
     .setColor(0x991928)
@@ -230,7 +241,7 @@ bot.on('message', message => {
     .addField("Armes :", "Faucille")
     .addField("Pouvoir :", "Soin (30 Mana),Resurection(150 Mana) Mana Booster(Seulement pour vos alliés)(Autant de point que vous booster)")
 
-    let emb8 = new Discord.RichEmbed({})
+    let emb8 = new Discord.RichEmbed({}) // Creation de l'embed
 
     .setTitle("Sorcier")
     .setColor(0x991928)
@@ -239,7 +250,7 @@ bot.on('message', message => {
     .addField("Armes :", "Sceptre")
     .addField("Pouvoir :", "Boule elementaire(40 Mana)(30 dêgats)")
 
-    let emb9 = new Discord.RichEmbed({})
+    let emb9 = new Discord.RichEmbed({}) // Creation de l'embed
 
     .setTitle("Chevalier")
     .setColor(0x991928)
@@ -248,7 +259,7 @@ bot.on('message', message => {
     .addField("Armes :", "Grande épée, Bouclier")
     .addField("Pouvoir :", "Booster de force (10 Mana)(+15 degats)")
 
-    let emb10 = new Discord.RichEmbed({})
+    let emb10 = new Discord.RichEmbed({}) // Creation de l'embed
 
     .setTitle("Voleur")
     .setColor(0x991928)
@@ -258,13 +269,13 @@ bot.on('message', message => {
     .addField("Pouvoir :", "Invisibilité (30 Mana)")
 
   message.channel.send(emb6).then(msg => {message.channel.send(emb7);}).then(msg => {message.channel.send(emb8);}).then(msg => {message.channel.send(emb9);}).then(msg => {message.channel.send(emb10);});
-
+  // Envoi de tout les embeds
   }
 
-  if (message.content === prefix + "carac") {
-    message.delete();
+  if (message.content === prefix + "carac") { // Commande d'aide sur les caractéristique des personnages
+    message.delete(); // Supression de la commande d'invocation
 
-    let emb11 = new Discord.RichEmbed({})
+    let carac = new Discord.RichEmbed({}) // Creation de l'embed
 
     .setTitle("Explication des caratéristiques des personnages")
     .setColor(0x066487)
@@ -275,7 +286,7 @@ bot.on('message', message => {
     .addField("Intelligence :", "L'intelligence est le reflet du potentiel intellectuel et du savoir du personnage : ses connaissances et sa capacité à les utiliser en toute circonstance. Elle définit la mémoire du personnage et lui sert à imaginer des plans, échafauder des théories à partir des éléments recueillis ou de posséder des acquis culturels importants. L’intelligence est primordiale chez les lanceurs de sorts car c’est grâce à son intelligence que le personnage pourra comprendre et mémoriser des sorts.")
     .addField("Intuition :", "Elle représente à la fois perception des choses et les pressentiments. Elle détermine la capacité à sentir les pièges ou à faire les bons choix. L’intuition résume à elle seule la sensibilité des cinq sens du personnage (vue, ouïe, toucher, odorat et goût) et sa capacité à interpréter ce qu'il détecte. Une bonne Perception est utile pour le combat à distance et permet de se sentir à l'aise n'importe où.")
     .addField("Charisme :", "Le charisme détermine la capacité d'un personnage à faire peur ou charmer un animal ou un individu, représente la prestance. Il représente le fait que vous impressionnez ou non les gens que vous croisez. Attention, le charisme n'est pas forcément positif. Il peut représenter la beauté chez une jeune danseuse, l’éloquence chez un diplomate, mais également la peur que fait ressentir un tyran.");
-    message.channel.send(emb11)
+    message.channel.send(carac) // Envoi de l'embed
   }
 
 });
